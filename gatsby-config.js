a8eefabf3d631d4914718fe4ecef235ecbb0bef7
Jack Westmore <jack@withtab.com>
2021-02-15 10:21:31 +0000

    + gatsby strapi source plugin, dotenv file for config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Pedro Younis Photography`,
@@ -11,6 +15,15 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.API_URL || `http://localhost:1337`,
+        contentTypes: ["article", "category", "writer"],
+        singleTypes: [`homepage`, `global`],
+        queryLimit: 1000,
       },
     },
     `gatsby-transformer-sharp`,
