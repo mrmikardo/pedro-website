2e0a94911761e76d6a354f3ba58e120f68aabcdd
Jack Westmore <jack@withtab.com>
2021-02-28 22:49:51 +0000

    + basic portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,15 +1,47 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = () => (
+const PortfolioPage = ({ data }) => (
   <Layout>
-    <SEO title="Contact" />
-    <div className="mt-10">
-      <p>Welcome to page 2</p>
+    <SEO title="Portfolio" />
+    <div className="my-20 grid grid-flow-row grid-cols-2 gap-3 gap-y-20">
+      {data.allStrapiGallery.nodes.map(node => {
+        return (
+          <div className="self-center p-3">
+            <Link to={"/gallery/" + node.slug}>
+              <Img
+                fluid={node.coverimage.localFile.childImageSharp.fluid}
+                style={{ cursor: "pointer" }}
+              />
+            </Link>
+          </div>
+        )
+      })}
     </div>
   </Layout>
 )
 
-export default ContactPage
+export const query = graphql`
+  query PortfolioPageQuery {
+    allStrapiGallery {
+      nodes {
+        slug
+        coverimage {
+          localFile {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default PortfolioPage
