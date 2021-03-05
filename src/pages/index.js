db3cb5f8ab8dacef1dbc58dda53708ba9a53313b
Jack Westmore <jack@withtab.com>
2021-03-05 23:03:21 +0000

    home page: make responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,43 @@ import SEO from "../components/seo"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <div className="my-20 grid grid-flow-row grid-cols-2 grid-rows-2 gap-3 gap-y-20">
+    {/* Mobile */}
+    <div className="md:hidden grid grid-flow-row grid-cols-1 gap-2 gap-y-1">
+      <div className="self-center">
+        <GatsbyImage
+          image={
+            data.strapiHomePage.image1.localFile.childImageSharp.gatsbyImageData
+          }
+          alt="Home page image 1"
+        />
+      </div>
+      <div className="self-center">
+        <GatsbyImage
+          image={
+            data.strapiHomePage.image2.localFile.childImageSharp.gatsbyImageData
+          }
+          alt="Home page image 2"
+        />
+      </div>
+      <div className="self-center">
+        <GatsbyImage
+          image={
+            data.strapiHomePage.image3.localFile.childImageSharp.gatsbyImageData
+          }
+          alt="Home page image 3"
+        />
+      </div>
+      <div className="self-center">
+        <GatsbyImage
+          image={
+            data.strapiHomePage.image4.localFile.childImageSharp.gatsbyImageData
+          }
+          alt="Home page image 4"
+        />
+      </div>
+    </div>
+    {/* Desktop */}
+    <div className="hidden my-20 grid grid-flow-row grid-cols-2 grid-rows-2 gap-3 gap-y-20">
       <div className="self-center">
         <GatsbyImage
           image={
