af7424430dc3429467b6be9d6d67e7bb9754f1b0
Jack Westmore <jack@withtab.com>
2021-03-06 15:53:58 +0000

    contact page: + social links
    Also adds material icons dependency

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,6 @@
 import React from "react"
+import InstagramIcon from "@material-ui/icons/Instagram"
+import MailOutlineIcon from "@material-ui/icons/MailOutline"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,6 +10,23 @@ const ContactPage = () => (
     <SEO title="Contact" />
     <div className="mt-10">
       <p>Welcome to page 2</p>
+      <div className="text-center font-bold leading-7">
+        <MailOutlineIcon />
+        &nbsp;
+        <a className="mb-2" href="mailto:pedroyounisg@gmail.com">
+          pedroyounisg@gmail.com
+        </a>
+        <br></br>
+        <InstagramIcon />
+        &nbsp;
+        <a
+          href="https://instagram.com/pygphoto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @pygphoto
+        </a>
+      </div>
     </div>
   </Layout>
 )
