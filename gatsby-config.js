f94789b3d889ea02a445a84dff9efc054874057f
Jack Westmore <jackwestmore@gmail.com>
2021-05-16 21:31:05 +0100

    update contact page
    Pull new ContactPage single type from Strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.API_URL || `http://localhost:1337`,
-        singleTypes: ["home-page"],
+        singleTypes: ["home-page", "contact-page"],
         contentTypes: ["gallery"],
         queryLimit: 1000,
       },
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,34 +1,64 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import InstagramIcon from "@material-ui/icons/Instagram"
 import MailOutlineIcon from "@material-ui/icons/MailOutline"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = () => (
+const ContactPage = ({ data }) => (
   <Layout>
     <SEO title="Contact" />
-    <div className="mt-10">
-      <p>Welcome to page 2</p>
-      <div className="text-center font-bold leading-7">
-        <MailOutlineIcon />
-        &nbsp;
-        <a className="mb-2" href="mailto:pedroyounisg@gmail.com">
-          pedroyounisg@gmail.com
-        </a>
-        <br></br>
-        <InstagramIcon />
-        &nbsp;
-        <a
-          href="https://instagram.com/pygphoto"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @pygphoto
-        </a>
+    <div className="grid grid-flow-row grid-cols-1 gap-x-8 justify-center my-20">
+      <div className="flex justify-center">
+        <GatsbyImage
+          image={
+            data.strapiContactPage.coverphoto.localFile.childImageSharp
+              .gatsbyImageData
+          }
+          alt="Pedro"
+        />
+      </div>
+      <div className="mt-10">
+        <div className="text-center font-bold leading-7">
+          <MailOutlineIcon />
+          &nbsp;
+          <a className="mb-2" href="mailto:pedroyounisg@gmail.com">
+            pedroyounisg@gmail.com
+          </a>
+          <br></br>
+          <InstagramIcon />
+          &nbsp;
+          <a
+            href="https://instagram.com/pygphoto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @pygphoto
+          </a>
+        </div>
       </div>
     </div>
   </Layout>
 )
+
+export const query = graphql`
+  query ContactPageQuery {
+    strapiContactPage {
+      coverphoto {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              width: 400
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ContactPage
