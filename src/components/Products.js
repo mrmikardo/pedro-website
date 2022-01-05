6b727d80df63329e761998bbe3c560a4dbba88b8
Jack Westmore <jackwestmore@gmail.com>
2022-01-05 13:57:29 +0000

    store: minor: - unused inline styles

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,14 +2,6 @@ import React from "react"
 import { graphql, StaticQuery } from "gatsby"
 import ProductCard from "./ProductCard"
 
-const containerStyles = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  justifyContent: "space-between",
-  padding: "1rem 0 1rem 0",
-}
-
 const Products = () => {
   return (
     <StaticQuery
