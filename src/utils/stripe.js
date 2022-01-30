67eaa1033e48059bb101903d4646d673d2f212b8
Jack Westmore <jackwestmore@gmail.com>
2022-01-30 23:30:14 +0000

    don't hardcode stripe keys (even dev/test ones!)

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -3,9 +3,7 @@ let stripePromise
 const getStripe = () => {
   if (!stripePromise) {
     // Stripe keys must be set in environment
-    stripePromise = loadStripe(
-      process.env.STRIPE_PUBLIC_KEY || `pk_test_s06Bi30n0QjpJpZ3P3cCG2Un`
-    )
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLIC_KEY)
   }
   return stripePromise
 }
