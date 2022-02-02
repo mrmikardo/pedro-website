6e4c15785c8b2d4176a84047030f230bc54c4423
Jack Westmore <jackwestmore@gmail.com>
2022-02-02 20:46:29 +0000

    mobile side bar: - redundant home link

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -60,10 +60,6 @@ const Sidebar = props => {
         >
           <div className="my-1">
             <span className="text-cyan-250">&#8212;&nbsp;</span>
-            <Link to="/">Home</Link>
-          </div>
-          <div className="my-1">
-            <span className="text-cyan-250">&#8212;&nbsp;</span>
             <Link to="/portraits">Portraits</Link>
           </div>
           <div className="my-1">
